a0217e1ece3a0861feeedfc9aab5ab64fee805b6
DAL185 <DAL185@pitt.edu>
2019-10-04 11:03:26 -0400

    Delete vhr and vuehr

--- vuehr/src/components/emp/EmpAdv.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>Senior Information</h1>
-  </div>
-</template>
--- vuehr/src/components/system/basic/JobTitleMana.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>Title Management</h1>
-  </div>
-</template>
-<script>
-
-</script>
